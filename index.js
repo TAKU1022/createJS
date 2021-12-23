d32e39a829c4d556f3c946657db098b2f94831af
Takumi <nase49946@gmail.com>
2021-12-23 15:44:39 +0900

    text operate

--- index.js
+++ index.js
@@ -9,17 +9,43 @@ class CanvasImage extends createjs.Bitmap {
     super();
 
     this.image = new Image();
+    this.isDrawn = false;
+    this.dx = 0;
+    this.dy = 0;
+
     this.image.onload = () => {
+      this.isDrawn = true;
       this.regX = this.image.width / 2;
       this.regY = this.image.height / 2;
-      this.x = this.image.width / 2;
-      this.y = this.image.height / 2;
+      this.x = this.regX;
+      this.y = this.regY;
     };
     stage.addChild(this);
   }
 }
 
+class CanvasText extends createjs.Text {
+  constructor() {
+    super();
+
+    this.isDrawn = false;
+    this.text = '';
+    this.fontSize = 30;
+    this.fontFamily = 'sans-serif';
+    this.font = `${this.fontSize}px ${this.fontFamily}`;
+    this.dx = 0;
+    this.dy = 0;
+    stage.addChild(this);
+  }
+
+  chageFont(fontFamily) {
+    this.fontFamily = fontFamily;
+    this.font = `${this.fontSize}px ${this.fontFamily}`;
+  }
+}
+
 const canvasImage = new CanvasImage();
+const canvasText = new CanvasText();
 
 const loadImage = (file) => {
   const reader = new FileReader();
@@ -50,24 +76,6 @@ downloadButton.addEventListener('click', () => {
   downloadCanvasImage();
 });
 
-const translate = (canvas, x, y) => {
-  canvas.x += x;
-  canvas.y += y;
-};
-
-const rotate = (canvas, degrees) => {
-  canvas.rotation += degrees;
-};
-
-const scale = (canvas, magnification) => {
-  canvas.scaleX += magnification;
-  canvas.scaleY += magnification;
-  if (canvas.scaleX < 0.1 && canvas.scaleY < 0.1) {
-    canvas.scaleX = 0.1;
-    canvas.scaleY = 0.1;
-  }
-};
-
 const translateButtons = [...document.querySelectorAll('[data-translate]')];
 const rotateButtons = [...document.querySelectorAll('[data-rotate]')];
 const scaleButtons = [...document.querySelectorAll('[data-scale]')];
@@ -75,21 +83,29 @@ const scaleButtons = [...document.querySelectorAll('[data-scale]')];
 translateButtons.forEach((translateButton) => {
   translateButton.addEventListener('click', (event) => {
     const target = event.currentTarget;
+    const canvas = target.dataset.canvas === 'image' ? canvasImage : canvasText;
+
+    if (!canvas.isDrawn) return;
+
     const direction = target.dataset.translate;
     const distance = 10;
 
     switch (direction) {
       case 'up':
-        translate(canvasImage, 0, -distance);
+        canvas.dy -= distance;
+        canvas.y -= distance;
         break;
       case 'down':
-        translate(canvasImage, 0, distance);
+        canvas.dy += distance;
+        canvas.y += distance;
         break;
       case 'left':
-        translate(canvasImage, -distance, 0);
+        canvas.dx -= distance;
+        canvas.x -= distance;
         break;
       case 'right':
-        translate(canvasImage, distance, 0);
+        canvas.dx += distance;
+        canvas.x += distance;
         break;
       default:
         alert('エラー');
@@ -100,13 +116,17 @@ translateButtons.forEach((translateButton) => {
 rotateButtons.forEach((rotateButton) => {
   rotateButton.addEventListener('click', (event) => {
     const target = event.currentTarget;
+    const canvas = target.dataset.canvas === 'image' ? canvasImage : canvasText;
+
+    if (!canvas.isDrawn) return;
+
     const direction = target.dataset.rotate;
     const degees = 15;
 
     if (direction === 'right') {
-      rotate(canvasImage, degees);
+      canvas.rotation += degees;
     } else if (direction === 'left') {
-      rotate(canvasImage, -degees);
+      canvas.rotation -= degees;
     } else {
       alert('エラー');
     }
@@ -116,15 +136,46 @@ rotateButtons.forEach((rotateButton) => {
 scaleButtons.forEach((scaleButton) => {
   scaleButton.addEventListener('click', (event) => {
     const target = event.currentTarget;
+    const canvas = target.dataset.canvas === 'image' ? canvasImage : canvasText;
+
+    if (!canvas.isDrawn) return;
+
     const direction = target.dataset.scale;
     const magnification = 0.2;
 
     if (direction === 'up') {
-      scale(canvasImage, magnification);
+      canvas.scaleX += magnification;
+      canvas.scaleY += magnification;
     } else if (direction === 'down') {
-      scale(canvasImage, -magnification);
+      canvas.scaleX -= magnification;
+      canvas.scaleY -= magnification;
+      if (canvas.scaleX < 0.1 && canvas.scaleY < 0.1) {
+        canvas.scaleX = 0.1;
+        canvas.scaleY = 0.1;
+      }
     } else {
       alert('エラー');
     }
+  });
+});
+
+const textarea = document.getElementById('textarea');
+const fontRadioButtons = [...document.getElementsByName('fonts')];
+
+textarea.addEventListener('input', (event) => {
+  event.currentTarget.value === ''
+    ? (canvasText.isDrawn = false)
+    : (canvasText.isDrawn = true);
+  canvasText.text = event.currentTarget.value;
+  canvasText.regX = canvasText.getBounds().width / 2;
+  canvasText.regY = canvasText.getBounds().height / 2;
+  canvasText.x = canvasText.regX + canvasText.dx;
+  canvasText.y = canvasText.regY + canvasText.dy;
+});
+
+fontRadioButtons.forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+    const fontFamilyName = event.currentTarget.value;
+    canvasText.chageFont(fontFamilyName);
   });
 });
